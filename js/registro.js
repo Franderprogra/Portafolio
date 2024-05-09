cbf7426c5df5bcfbcc9d3acafa6f5ac0550f5027
Franderprogra <franderprogra@gmail.com>
2024-05-09 13:17:06 -0600

    registro2

---
+++ js/registro.js
@@ -0,0 +1,28 @@
+let ID=document.getElementById("ID")
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let registroUser= [];
+ function registrar(){
+    registroUser= JSON.parse(localStorage.getItem("registro")) || [];
+    encontrado= registroUser.find (usuario=> usuario.ID==ID.value &&  usuario.email==email.value)
+    console.log(encontrado)
+    if (encontrado!=undefined) {
+        alert("este usuario ya esta registrado")
+        return;
+        
+    }
+    if (ID != "" && email != "" && password != "") {  
+         data = {
+            ID: ID.value,
+            email: email.value,
+            password: password.value
+        }
+        
+        registroUser.push(data)
+
+        localStorage.setItem("registro",JSON.stringify(registroUser))
+        
+    }
+    
+ }
+ 
